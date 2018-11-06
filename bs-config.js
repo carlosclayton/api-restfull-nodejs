5619d2be78f8168e881fffd6d967d1d417f39af1
Carlos Clayton <carlos.clayton@gmail.com>
2018-11-06 12:40:17 -0300

    Adicionando gitignore

--- bs-config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/// Export configuration options
-module.exports = {
-    "files" : "./src/**/*.{js, html, css}",
-    "server" : { "baseDir" : "./src" },
-    "browser" : ["chrome"]
-}
